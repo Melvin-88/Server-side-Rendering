003e5a23287c9f59a65bc5395da39727d101211f
Melvin-88 <taisen747@gmail.com>
2017-05-22 16:52:31 +0300

    add

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ import {RoutingContext, match} from 'react-router';
 import routes from './routes';
 
 const env = process.env;
-var baseUrl = (env.USER && env.USER.indexOf('root') > -1) ? 'http://chicagowebapp.com/' : `${env.npm_package_config_appWebpackBaseUrl}`;
+var baseUrl = (env.USER && env.USER.indexOf('root') > -1) ? 'http://serverrender.4-com.pro/' : `${env.npm_package_config_appWebpackBaseUrl}`;
 
 
 const assetsPath = `${baseUrl}/${env.npm_package_version}`;
@@ -63,6 +63,7 @@ app.get('/', (req, res, next) => {
         ].join('\n');
         res.setHeader('Content-Type', 'text/html');
         res.send(html);
+        console.log(html);
     });
 });
 
