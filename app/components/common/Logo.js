3a72b123a0f5e2d60b174dda493d29059bd07ec5
Melvin-88 <taisen747@gmail.com>
2017-05-22 15:53:45 +0300

    add

--- app/components/common/Logo.js
+++ app/components/common/Logo.js
@@ -5,11 +5,12 @@ import {Link} from 'react-router';
 export default class Logo extends React.Component {
     render() {
         return (
-            <div style={styles.logoWrapper}>
-                <Link to="/">
-                    <img style={styles.base} src="https://s3-us-west-2.amazonaws.com/chicagoview/icons/react-logo.png"/>
-                </Link>
-            </div>
+            {/*<div style={styles.logoWrapper}>*/}
+                {/*<Link to="/">*/}
+                    {/*<img style={styles.base} src="https://s3-us-west-2.amazonaws.com/chicagoview/icons/react-logo.png"/>*/}
+                {/*</Link>*/}
+            {/*</div>*/}
+            <div>111</div>
         )
     }
 };
