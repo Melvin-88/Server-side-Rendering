25d2719c43eca0588f62d749d72023c534ff1b0c
Melvin-88 <taisen747@gmail.com>
2017-05-22 17:03:49 +0300

    add

--- app/components/App.js
+++ app/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router';
 import Helmet from "react-helmet";
 import Aside from "./common/Aside";
