2c9e398e83b116d838d2faa02e2fe702a3683658
Melvin-88 <taisen747@gmail.com>
2017-05-23 09:56:15 +0300

    add

--- app/server.js
+++ app/server.js
@@ -11,7 +11,8 @@ const env = process.env;
 var baseUrl = (env.USER && env.USER.indexOf('root') > -1) ? 'http://serverrender.4-com.pro/' : `${env.npm_package_config_appWebpackBaseUrl}`;
 
 
-const assetsPath = `${baseUrl}/${env.npm_package_version}`;
+const assetsPath = `${baseUrl}${env.npm_package_version}`;
+
 const publicPath = path.resolve('../public');
 
 let app = express();
